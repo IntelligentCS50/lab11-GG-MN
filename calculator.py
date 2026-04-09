3841d81a70ffe34435dff05453979359d85191b7
IntelligentCS50 <143908532+IntelligentCS50@users.noreply.github.com>
2026-04-09 19:15:49 -0400

    Add repo info and partner details

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/IntelligentCS50/lab11-GG-MN
+# Partner 1: Gideon Goldmann
+# Partner 2: Chantelle Monae Nera
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
